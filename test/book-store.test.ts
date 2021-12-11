0ea7efaa5a9ac0469093f1ca158dcbcd2470e60f
Ivo Raisr <i.raisr@rheagroup.com>
2021-12-11 12:35:18 +0100

    Initial repository setup for AWS CDK

---
+++ test/book-store.test.ts
@@ -0,0 +1,17 @@
+// import * as cdk from 'aws-cdk-lib';
+// import { Template } from 'aws-cdk-lib/assertions';
+// import * as BookStore from '../lib/book-store-stack';
+
+// example test. To run these tests, uncomment this file along with the
+// example resource in lib/book-store-stack.ts
+test('SQS Queue Created', () => {
+//   const app = new cdk.App();
+//     // WHEN
+//   const stack = new BookStore.BookStoreStack(app, 'MyTestStack');
+//     // THEN
+//   const template = Template.fromStack(stack);
+
+//   template.hasResourceProperties('AWS::SQS::Queue', {
+//     VisibilityTimeout: 300
+//   });
+});
